495b68b3aa76741a2eeb7d47456d2f07ae2af634
ayush.779gupta@gmail.com <ayush.779gupta@gmail.com>
2026-09-17 15:11:57 +0000

    Add `vfjyghn` transformation; update AIRLINES_AIRLINES source and remove schedules from `p2` and `p3` pipelines

--- snow2/pipelines/p2.py
+++ snow2/pipelines/p2.py
@@ -1,16 +1,6 @@
 from prophecy_pipeline_sdk.graph import *
 from prophecy_pipeline_sdk.properties import *
-Schedules = [Schedule(
-               Name = "sf2",
-               emails = ["g.ayush@prophecy.io"],
-               emailOnStart = True,
-               emailOnFailure = True,
-               emailOnSuccess = True,
-               versionMode = "latest",
-               cron = "0 0/1 * * * ? *",
-               timezone = "Asia/Kolkata"
-             )]
-args = PipelineArgs(label = "p2", version = 1, auto_layout = False, schedules = Schedules)
+args = PipelineArgs(label = "p2", version = 1, auto_layout = False)
 
 with Pipeline(args) as pipeline:
     aircraft_fleet_snowflake = Process(
--- snow2/pipelines/p3.py
+++ snow2/pipelines/p3.py
@@ -1,16 +1,6 @@
 from prophecy_pipeline_sdk.graph import *
 from prophecy_pipeline_sdk.properties import *
-Schedules = [Schedule(
-               Name = "schedule_fis",
-               emails = ["g.ayush@prophecy.io"],
-               emailOnStart = True,
-               emailOnFailure = True,
-               emailOnSuccess = True,
-               versionMode = "latest",
-               cron = "0 0/1 * * * ? *",
-               timezone = "Asia/Kolkata"
-             )]
-args = PipelineArgs(label = "p3", version = 1, auto_layout = False, schedules = Schedules)
+args = PipelineArgs(label = "p3", version = 1, auto_layout = False)
 
 with Pipeline(args) as pipeline:
     p3__reformat_1 = Process(name = "p3__Reformat_1", properties = ModelTransform(modelName = "p3__Reformat_1"))
